e245556b2265b6b74819c4a00f0a49e846a5e55c
Sushant-sushii <kumar1sushantsingh.com>
2026-07-08 17:44:07 +0530

    cc

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const userModel=require('../models/User.model');
+const userModel=require('../models/user.model');
 const jwt=require('jsonwebtoken');
 const bcrypt=require('bcrypt');
 const { response } = require('../app');
