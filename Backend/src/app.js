074e3b20add1e4f3bdc40938bcc1914ec349b3ce
Sushant-sushii <kumar1sushantsingh.com>
2026-07-08 18:05:44 +0530

    aa

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app=express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://impression-frontend-xi.vercel.app'
+  'https://impression-frontend-xi.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
